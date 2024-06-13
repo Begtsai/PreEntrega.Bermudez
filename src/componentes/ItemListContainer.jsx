e151280115b8e82ba0bbe566a4f250f565be9f62
Airam Bermudez <begtsai@gmail.com>
2024-06-13 18:57:20 -0300

    reentrega

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ItemList } from './ItemList';
+import datos from '../datos/equipos.json';
 
 export const ItemListContainer = () => {
 
--- src/componentes/header/NavBar.jsx
+++ src/componentes/header/NavBar.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Carrito from './Carrito'
+import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid menu">
-                <a className="navbar-brand" href="#">Equipos</a>
+                <Link to="/" className="navbar-brand" href="#">Equipos</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="#">Alquilar</a>
-                    <a className="nav-link" href="#">Contacto</a>
+                    <Link to="/" className="nav-link" href="#">Contacto</Link>
                 </div>
               </div> 
               <Carrito />
